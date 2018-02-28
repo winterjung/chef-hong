27b40326aa52036fda0f217dfd23702092f40ca9
jungwinter <wintermy201@gmail.com>
2018-02-28 11:26:04 +0900

    Chef Hong: Add chatter to shell context

--- manage.py
+++ manage.py
@@ -4,13 +4,14 @@ import os
 from flask_script import Manager, Shell
 
 from app import create_app, db
+from app.api import chatter
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 
 
 def make_shell_context():
-    return dict(app=app, db=db)
+    return dict(app=app, db=db, chatter=chatter)
 
 
 manager.add_command('shell', Shell(make_context=make_shell_context))
