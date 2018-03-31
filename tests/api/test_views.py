1a3eb28fb1846fb001fe46f665e35cd00b80d571
jungwinter <wintermy201@gmail.com>
2018-03-31 21:44:28 +0900

    Tests: Move helper class to conftest

--- tests/api/test_views.py
+++ tests/api/test_views.py
@@ -1,26 +1,20 @@
-import json
-import random
-import string
 import pytest
 from flask import url_for
 
 
 class TestKeyboard:
-    def test_keyboard(self, client, mocker):
-        mocker.patch('app.api.chatter.logger')
+    def test_keyboard(self, client):
         url = url_for('api.keyboard')
-        res = Client(client).url(url).get()
+        res = client.url(url).get()
         assert res.json['buttons'] == ['오늘의 식단', '다른 식단 보기', '다른 기능']
 
 
 class TestMessage:
     @classmethod
     @pytest.fixture(autouse=True)
-    def set_up(self, client, mocker):
-        mocker.patch('app.api.chatter.logger')
-        mocker.patch('app.api.menu.day_to_weekday', **{'return_value': 0})
+    def set_up(self, client):
         url = url_for('api.message')
-        self.client = Client(client).url(url)
+        self.client = client.url(url)
 
     def test_today(self):
         res = self.client.post('오늘의 식단')
@@ -29,14 +23,14 @@ class TestMessage:
             .contain('오늘의 간략한 식단', '식당')
             .keyboard(['전체 식단', '점심', '신기숙사', '취소']))
 
-    def test_other(self, client):
+    def test_other(self):
         res = self.client.post('다른 식단 보기')
         (res
             .status(200)
             .contain('내일의 간략한 식단', '식당')
             .keyboard(['내일의 전체 식단', '내일의 신기숙사', '이번주 식단', '취소']))
 
-    def test_etc(self, client):
+    def test_etc(self):
         res = self.client.post('다른 기능')
         (res
             .status(200)
@@ -44,7 +38,7 @@ class TestMessage:
             .msg('text')
             .keyboard(['자기소개', '추가될 기능', '취소']))
 
-    def test_today_step2(self, client):
+    def test_today_step2(self):
         res = self.client.post('오늘의 식단', '전체 식단')
         (res
             .status(200)
@@ -63,7 +57,7 @@ class TestMessage:
             .contain('기숙사')
             .home())
 
-    def test_other_step2(self, client):
+    def test_other_step2(self):
         res = self.client.post('다른 식단 보기', '내일의 전체 식단')
         (res
             .status(200)
@@ -82,21 +76,21 @@ class TestMessage:
             .contain('이번주')
             .home())
 
-    def test_etc_intro(self, client):
+    def test_etc_intro(self):
         res = self.client.post('다른 기능', '자기소개')
         (res
             .status(200)
             .msg('text')
             .home())
 
-    def test_etc_roadmap(self, client):
+    def test_etc_roadmap(self):
         res = self.client.post('다른 기능', '추가될 기능')
         (res
             .status(200)
             .msg('text')
             .home())
 
-    def test_cancel(self, client):
+    def test_cancel(self):
         res = self.client.post('오늘의 식단', '취소')
         (res
             .status(200)
@@ -104,77 +98,3 @@ class TestMessage:
             .msg('text')
             .home())
 
-
-class Client:
-    def __init__(self, client):
-        self.client = client
-
-    def url(self, url):
-        self.url = url
-        return self
-
-    def get(self):
-        self.response = self.client.get(self.url)
-        return Response(self.response)
-
-    def post(self, *args):
-        user_key = self.random_user()
-        for content in args:
-            payload = self.data(content, user_key)
-            self.response = self.client.post(
-                self.url,
-                data=json.dumps(payload),
-                content_type='application/json'
-            )
-
-        return Response(self.response)
-
-    def random_user(self, n=16):
-        charset = string.ascii_uppercase + string.digits
-        return ''.join(random.choices(charset, k=n))
-
-    def data(self, content, user_key, message_type='text'):
-        return {
-            'user_key': user_key,
-            'type': message_type,
-            'content': content,
-        }
-
-
-class Response:
-    def __init__(self, data):
-        self.response = data
-        self.json = self.response.json
-
-    def status(self, code):
-        assert self.response.status_code == code
-        return self
-
-    def contain(self, *args):
-        for target in args:
-            msg = self.json['message']
-            text = msg.get('text', '')
-            photo = msg.get('photo', {})
-            button = msg.get('message_button', {})
-
-            in_text = target in text
-            in_photo = target in photo.get('url', '')
-            in_button_label = target in button.get('label', '')
-            in_button_url = target in button.get('url', '')
-
-            assert any([in_text, in_photo, in_button_label, in_button_url])
-        return self
-
-    def msg(self, *args):
-        for msg in (args):
-            assert msg in self.json['message']
-        return self
-
-    def keyboard(self, buttons):
-        assert buttons == self.json['keyboard']['buttons']
-        return self
-
-    def home(self):
-        buttons = ['오늘의 식단', '다른 식단 보기', '다른 기능']
-        assert buttons == self.json['keyboard']['buttons']
-        return self
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,6 @@
+import json
+import random
+import string
 import pytest
 
 from app import create_app
@@ -10,6 +13,84 @@ def app():
 
 
 @pytest.yield_fixture(scope='function')
-def client(app):
+def client(app, mocker):
     with app.test_client() as client:
+        mocker.patch('app.api.chatter.logger')
+        mocker.patch('app.api.menu.day_to_weekday', **{'return_value': 0})
+        client = Client(client)
         yield client
+
+
+class Client:
+    def __init__(self, client):
+        self.client = client
+
+    def url(self, url):
+        self.url = url
+        return self
+
+    def get(self):
+        self.response = self.client.get(self.url)
+        return Response(self.response)
+
+    def post(self, *args):
+        user_key = self.random_user()
+        for content in args:
+            payload = self.data(content, user_key)
+            self.response = self.client.post(
+                self.url,
+                data=json.dumps(payload),
+                content_type='application/json'
+            )
+
+        return Response(self.response)
+
+    def random_user(self, n=16):
+        charset = string.ascii_uppercase + string.digits
+        return ''.join(random.choices(charset, k=n))
+
+    def data(self, content, user_key, message_type='text'):
+        return {
+            'user_key': user_key,
+            'type': message_type,
+            'content': content,
+        }
+
+
+class Response:
+    def __init__(self, data):
+        self.response = data
+        self.json = self.response.json
+
+    def status(self, code):
+        assert self.response.status_code == code
+        return self
+
+    def contain(self, *args):
+        for target in args:
+            msg = self.json['message']
+            text = msg.get('text', '')
+            photo = msg.get('photo', {})
+            button = msg.get('message_button', {})
+
+            in_text = target in text
+            in_photo = target in photo.get('url', '')
+            in_button_label = target in button.get('label', '')
+            in_button_url = target in button.get('url', '')
+
+            assert any([in_text, in_photo, in_button_label, in_button_url])
+        return self
+
+    def msg(self, *args):
+        for msg in (args):
+            assert msg in self.json['message']
+        return self
+
+    def keyboard(self, buttons):
+        assert buttons == self.json['keyboard']['buttons']
+        return self
+
+    def home(self):
+        buttons = ['오늘의 식단', '다른 식단 보기', '다른 기능']
+        assert buttons == self.json['keyboard']['buttons']
+        return self
