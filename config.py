8d8e9f566b09b708ecf8ff276112724578d161dc
jungwinter <wintermy201@gmail.com>
2018-04-14 20:41:20 +0900

    Config: Add cautions

--- config.py
+++ config.py
@@ -7,10 +7,7 @@ basedir = pathlib.Path(__file__).cwd()
 class Config:
     APP_NAME = 'Chef Hong'
 
-    if os.environ.get('SECRET_KEY'):
-        SECRET_KEY = os.environ.get('SECRET_KEY')
-    else:
-        SECRET_KEY = os.urandom(24)
+    SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(24))
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
@@ -25,20 +22,34 @@ class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
         'sqlite:///' + str(basedir / 'data-dev.sqlite')
 
+    @classmethod
+    def init_app(cls, app):
+        super().init_app(app)
+        print('This is development config')
+
 
 class TestingConfig(Config):
     TESTING = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
         'sqlite:///' + str(basedir / 'data-test.sqlite')
 
+    @classmethod
+    def init_app(cls, app):
+        super().init_app(app)
+        print('This is testing config')
+
 
 class ProductionConfig(Config):
+    DEBUG = False
+    TESTING = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + str(basedir / 'data.sqlite')
 
     @classmethod
     def init_app(cls, app):
         super().init_app(app)
+        print('THIS IS DEVELOPMENT CONFIG!')
 
 
 class UnixConfig(ProductionConfig):
