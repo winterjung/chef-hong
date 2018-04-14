3470c81522888a8ee1fd7148527afcdf14570d65
jungwinter <wintermy201@gmail.com>
2018-04-14 20:40:54 +0900

    DB: Init with SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,19 @@
 import pathlib
-from flask import Flask
-from pony.orm import Database
 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from config import config
 
 basedir = pathlib.Path(__file__).cwd()
-db = Database()
+db = SQLAlchemy()
 
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     config[config_name].init_app(app)
-    db.bind(provider='sqlite', filename=':memory:')
-    db.generate_mapping(create_tables=True)
+    db.init_app(app)
 
     from app.api import api as api_blueprint
     from app.util import util as util_blueprint
--- app/models.py
+++ app/models.py
@@ -1,8 +1,22 @@
-from pony.orm import Required
+from datetime import datetime
 
 from app import db
 
 
-class User(db.Entity):
-    user_key = Required(str)
-    nickname = Required(str)
+class ModelMixin:
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(
+        db.DateTime,
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow)
+
+
+class User(ModelMixin, db.Model):
+    user_key = db.Column(db.String, nullable=False, unique=True)
+
+    def __init__(self, user_key):
+        self.user_key = user_key
+
+    def __repr__(self):
+        return '<User {} {}>'.format(self.id, self.user_key)
